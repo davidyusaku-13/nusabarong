2a4dac2523d9b4132d69b576b057174775af8e6e
David Yusaku <davidyusaku13@gmail.com>
2025-02-08 12:27:34 +0700

    added Featured Cars section

--- components/FeaturedCard.tsx
+++ components/FeaturedCard.tsx
@@ -7,7 +7,7 @@ export default function FeaturedCard() {
       <div className="flex">
         <Image src={"/img/datsun.jpeg"} width={300} height={300} alt="datsun" />
         <p className="bg-[#AF0000] w-[200px] p-6 text-justify text-sm">
-          Lorem Ipsum is simply dummy text of the printing and typesetting
+          Lorem Ipsum is simply dummy text of the printing and typesetting
           industry. Lorem Ipsum has been the industry's standard dummy text ever
           since the 1500s, when an unknown printer took a galley of type and
           scrambled it to make a type specimen book.
