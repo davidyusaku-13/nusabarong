20b5e755603df22886170d4cb53458756c6fad08
David Yusaku <davidyusaku13@gmail.com>
2025-02-08 12:32:27 +0700

    added Featured Cars section

--- components/Featured.tsx
+++ components/Featured.tsx
@@ -4,7 +4,8 @@ import FeaturedCard from "./FeaturedCard";
 export default function Featured() {
   return (
     <>
-      <div className="bg-black text-white flex flex-col w-full min-h-[769px] pb-8">
+      {/* min height 737 + 32(pb-8) = 769px */}
+      <div className="bg-black text-white flex flex-col w-full min-h-[737px] pb-8">
         <div className="mt-24 justify-center items-center text-center">
           <p className=" text-[64px] font-extrabold">FEATURED PRODUCTS</p>
         </div>
